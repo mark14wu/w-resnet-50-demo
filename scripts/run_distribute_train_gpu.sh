b2706ff011ff004cc1302a6fdfe91c6c1864a0c0
Hao Wu <mark14@mail.ustc.edu.cn>
2021-01-15 04:23:05 +0800

    modify running script

--- scripts/run_distribute_train_gpu.sh
+++ scripts/run_distribute_train_gpu.sh
@@ -7,8 +7,6 @@ cp *.sh ./train_parallel
 cp -r ../src ./train_parallel
 cd ./train_parallel || exit
 
-# export LD_LIBRARY_PATH=/usr/local/python-3.7.5/lib
-
-/usr/local/openmpi-4.0.3/bin/mpirun --allow-run-as-root --mca btl_tcp_if_include ens11f0 -n 2 -H gpu11-docker:2 --output-filename log_output --merge-stderr-to-stdout \
-  python3 train.py --net="resnet50" --dataset="cifar10" --run_distribute=True \
+/usr/local/openmpi-4.0.3/bin/mpirun --allow-run-as-root --mca btl_tcp_if_include ens11f0 -x LD_LIBRARY_PATH=/usr/local/python-3.7.5/lib -x NCCL_SOCKET_IFNAME=ens11f0 -n 8 -H gpu3-docker:2,gpu5-docker:2,gpu7-docker:2,gpu11-docker:2 --output-filename log_output --merge-stderr-to-stdout \
+  python train.py --net="resnet50" --dataset="cifar10" --run_distribute=True \
     --device_target="GPU" --dataset_path="/data/cifar-10-batches-bin/" &> log &
